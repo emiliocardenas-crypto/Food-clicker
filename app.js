d426e2fa3d763032e64aa00a69309218542f70cc
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-05-13 22:55:26 +0000

    the

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let numclicks=0
 let clickpower=1
 
+// let cakelink = 
+let cereallink = "https://static.vecteezy.com/system/resources/previews/048/111/985/non_2x/bowl-of-cereal-free-png.png"
+let ogfood= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0F6uHv-XTcCzNL3uLK_izPjUkDbeQbmHTZw&s"
 function showclicks(){
     document.getElementById("numclick").textContent="Number of clicks: " + numclicks
 }
@@ -8,6 +11,20 @@ function showclicks(){
 function incresclicks(){
     numclicks=(clickpower + numclicks)
     showclicks()
+
+
+    if (numclicks>=10000){
+
+         document.getElementById("thegoodfood").src= cereallink
+         document.getElementById("thegoodfood").style.height = "220px"
+         document.getElementById("thegoodfood").style.width = "200px"
+    }
+    else{
+          document.getElementById("thegoodfood").src= ogfood
+         document.getElementById("thegoodfood").style.height = "220px"
+         document.getElementById("thegoodfood").style.width = "200px"
+    }
+    }
 }
 
 showclicks()
